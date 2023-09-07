5475530d85db47317091b6ce7fd420f860ed1358
Ajay Kumar M <ajaykumarm.be23@uceou.edu>
2023-09-07 11:00:39 +0530

    responsive checkpoints added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,51 +7,54 @@ import { BsTwitter } from "react-icons/bs";
 function Footer() {
   return (
     <div className="Footer">
-        <div className="footer-container">
-      <div className="logo-container">
-        <div className="logo">
-          <img src={logo} alt="logo" />
+      <div className="footer-container">
+        <div className="logo-container">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
         </div>
-      </div>
-      <div className="contact-us">
-        <h3>Contact Us</h3>
-        <ul className="contact">
-          <li>
-            Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM
-            Market, XYZ-343434
-          </li>
-          <li>example2020@gmail.com</li>
-          <li>(904) 443-0343</li>
-        </ul>
-      </div>
-      <div className="more">
-        <h3 className="title">More</h3>
-        <ul className="company">
-          <li className="hover-link">About Us</li>
-          <li className="hover-link">Products</li>
-          <li className="hover-link">Careers</li>
-          <li className="hover-link">Contact Us</li>
-        </ul>
-      </div>
-      <div className="social-links">
-        <div className="social-top">
-          <h3>Social Links</h3>
-          <ul className="follow">
-            <li className="hover-link" id="fb">
-              <AiFillFacebook />
+        <div className="contact-us">
+          <h3>Contact Us</h3>
+          <ul className="contact">
+            <li>
+              Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM
+              Market, XYZ-343434
             </li>
-            <li className="hover-link">
-              <AiOutlineInstagram />
-            </li>
-            <li className="hover-link">
-              <BsTwitter />
-            </li>
+            <li>example2020@gmail.com</li>
+            <li>(904) 443-0343</li>
           </ul>
         </div>
-        <div className="social-bottom">
-        <p className="copyright">Copyright {new Date().getFullYear()} © <strong>Food Truck Example</strong> </p>
+        <div className="more">
+          <h3 className="title">More</h3>
+          <ul className="company">
+            <li className="hover-link">About Us</li>
+            <li className="hover-link">Products</li>
+            <li className="hover-link">Careers</li>
+            <li className="hover-link">Contact Us</li>
+          </ul>
         </div>
-      </div>
+        <div className="social-links">
+          <div className="social-top">
+            <h3>Social Links</h3>
+            <ul className="follow">
+              <li className="hover-link" id="fb">
+                <AiFillFacebook />
+              </li>
+              <li className="hover-link">
+                <AiOutlineInstagram />
+              </li>
+              <li className="hover-link">
+                <BsTwitter />
+              </li>
+            </ul>
+          </div>
+          <div className="social-bottom">
+            <p className="copyright">
+              Copyright {new Date().getFullYear()} ©{" "}
+              <strong>Food Truck Example</strong>{" "}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import './Product.scss'
-import dummyImg from '../../assets/pizza.avif'
 
 function Product({item}) {
   return (
-    <div className='product-container'>
-        <div className="image-container">
-            <img src={item.url} alt={item.title} />
+    <div className='Product'>
+      <div className="product-container">
+        <div className="product-img">
+          <div className="image" >
+            <img src={item.url} alt={item.title} id='img' />
+          </div>
         </div>
-        <h3 className='text'>{item.title}</h3>
-        <p>{item.desc}</p>
-        <button className="btn-primary">Read More</button>
+        <div className="product-info">
+          <h3 className="title">
+            {item.title}
+          </h3>
+          <p className="desc">
+            {item.desc}
+          </p>
+          <button className="btn-primary">Read More</button>
+        </div>
+      </div>
     </div>
   )
 }
